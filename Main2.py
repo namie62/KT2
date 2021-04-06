631ca738e2eec1b8f0914794da123bfc5f02656b
Nadja Mielke <namie62@t-online.de>
2021-04-06 16:36:24 +0200

    Fehlermeldungen müssen noch gemacht werden, ansonsten vergleicht er alle brav miteinander. Anschließend GUI

--- Main2.py
+++ Main2.py
@@ -4,13 +4,15 @@ from time import sleep
 import config
 from Kabel import KABEL
 import RPi.GPIO as gpio
+from pprint import pprint
 
 def setup():
     # Instanz von SMbus anlegen, ist nur einmal notwendig
+    litzenzahl = 4
     bus = smbus.SMBus(1)
     gpio.setmode(gpio.BCM)
     gpio.setwarnings(False)
-    return bus
+    return bus, litzenzahl
 
 # # lässt testhalber alle LEDs von Reihe B aus für 3 Sek leuchten am übergebenen Stein
 # def led_test_Reihe_B(stein, pin):
@@ -36,16 +38,16 @@ def setup():
 #     h = hex(i)
 #     return h
 def setze_pin_auf_low(adresse, aktion, pin):
-    setze_pullup_und_output()
+    setze_pullup_und_input()
     setze_pi_pins_zurueck()
     if aktion == "A":
-        bus.write_byte_data(config.ADRESSEN_STEINE.get(adresse),config.AKTION.get("DIRA"), config.PINNUMMERN.get(pin))#config.PINNUMMERN.get(pin))
-        bus.write_byte_data(config.ADRESSEN_STEINE.get(adresse),config.AKTION.get("OUTA"), config.PINNUMMERN.get(pin))#config.PINNUMMERN.get(pin))
+        bus.write_byte_data(config.ADRESSEN_STEINE.get(adresse),config.AKTION.get("DIRA"), config.PINNUMMERN.get(pin))
+        bus.write_byte_data(config.ADRESSEN_STEINE.get(adresse),config.AKTION.get("OUTA"), config.PINNUMMERN.get(pin))
     elif aktion=="B":
-        bus.write_byte_data(config.ADRESSEN_STEINE.get(adresse),config.AKTION.get("DIRB"), config.PINNUMMERN.get(pin))#config.PINNUMMERN.get(pin))
-        bus.write_byte_data(config.ADRESSEN_STEINE.get(adresse),config.AKTION.get("OUTB"), config.PINNUMMERN.get(pin))#config.PINNUMMERN.get(pin))
+        bus.write_byte_data(config.ADRESSEN_STEINE.get(adresse),config.AKTION.get("DIRB"), config.PINNUMMERN.get(pin))
+        bus.write_byte_data(config.ADRESSEN_STEINE.get(adresse),config.AKTION.get("OUTB"), config.PINNUMMERN.get(pin))
     else:
-        pi_gpio_setup_as_out_and_high(int(pin))
+        pi_gpio_setup_as_out_and_low(int(pin))
     #sleep(0.5)
 
 def string_zu_befehl(string):
@@ -54,22 +56,77 @@ def string_zu_befehl(string):
     pin = string[2:4]
     return adresse, aktion, pin
 
-def checke_werte():#adresse, aktion):
-    werte1a = bus.read_byte_data(0x21, 0x12)
-    werte2a = bus.read_byte_data(0x26, 0x12)
-    werte3a = bus.read_byte_data(0x22, 0x12)
-    werte4a = bus.read_byte_data(0x24, 0x12)
-    werte5a = bus.read_byte_data(0x20, 0x12)
-    werte1b = bus.read_byte_data(0x21, 0x13)
-    werte2b = bus.read_byte_data(0x26, 0x13)
-    werte3b = bus.read_byte_data(0x22, 0x13)
-    werte4b = bus.read_byte_data(0x24, 0x13)
-    werte5b = bus.read_byte_data(0x20, 0x13)
-    lese_gpios_aus()    
-    print("stein1a:", hex(werte1a),"stein2a:", hex(werte2a),"stein3a:", hex(werte3a),"stein4a:", hex(werte4a),"stein5a:", hex(werte5a))
-    print("stein1b:", hex(werte1b),"stein2b:", hex(werte2b),"stein3b:", hex(werte3b),"stein4b:", hex(werte5b),"stein5b:", hex(werte5b))
+def checke_auf_durchgang(i):
+    erstelle_dic(i) # erstellt einmal das Dictionary, indem alle pins die low sind aufgeführt werden
+    for stein in config.ADRESSEN_STEINE:
+        adresse = config.ADRESSEN_STEINE[stein] # holt sich adresse des jeweiligen steins
+        wertea = lese_werte(stein, adresse , "A") #Werte von jedem stein von reihe a
+        werteb = lese_werte(stein, adresse , "B")
+
+        bin_reihe_a = rechne_werte_um_in_bin(wertea)  # hex wert in bin string umwandeln
+        bin_reihe_b = rechne_werte_um_in_bin(werteb)
+        
+        low_pinsa = ermittle_low_pins_aus_bin(bin_reihe_a) # bin string durchgehen und 0en
+        low_pinsb = ermittle_low_pins_aus_bin(bin_reihe_b)# die 0en dann in liste abspeichern
+                
+        for pin in low_pinsa: # Um jeden der Low pins den passenden String packen und ins Dictionary abspeichern
+            a = str(stein) + "A" + str(pin)
+            packe_low_pins_in_dic(i, a)
+        for pin in low_pinsb:
+            b = str(stein) + "B" + str(pin)
+            packe_low_pins_in_dic(i, b)
+            
+    gpio_liste = lese_gpios_aus() #hier gpio low pins finden und ebenfalls in liste packen
+        
+    low_gpio_index = ermittle_low_gpio_index(gpio_liste)
+    for gpio in low_gpio_index:
+        c = "r" + "p" + str(config.GPIOS[gpio])
+        packe_low_pins_in_dic(i,c)
+
+def ermittle_low_gpio_index(gpio_liste):
+    wdh = len(gpio_liste)
+    low_gpios = []
+    for i in range(wdh):
+        gpio_liste[i] = str(gpio_liste[i])
+        if gpio_liste[i] == "0":
+            low_gpios.append(i)
+    return low_gpios
+
+def hole_gpio_nummern(low_gpio_index):
+    gpio_nummern = []
+    for i in low_gpio_index:
+        gpio_nummern.append(config.GPIOS[i])
+    return gpio_nummern
+            
+
+def lese_werte(i, adresse, reihe):
+    if reihe  == "A":
+        werte = bus.read_byte_data(adresse, 0x12)
+    else: 
+        werte = bus.read_byte_data(adresse, 0x13)
+    return werte
+   
+def rechne_werte_um_in_bin(wert):
+    h = str(hex(wert))
+    scale = 16
+    num_of_bits= 8
+    b = bin(int(h, scale))[2:].zfill(num_of_bits)
+    return b
+
+def ermittle_low_pins_aus_bin(bin_reihe_a):#, bin_reihe_b, stein, i):
+    low_pins = []
+    for buchstabennummer in range(len(bin_reihe_a)):
+        if bin_reihe_a[buchstabennummer] == "0":
+            low_pins.append(8-buchstabennummer)
+    return low_pins
+
+def erstelle_dic(i):
+        config.VERGLEICH_PINS[i+1] = []
     
-def pi_gpio_setup_as_out_and_high(pin):
+def packe_low_pins_in_dic(i, low_pin):
+    config.VERGLEICH_PINS[i+1].append(low_pin) 
+        
+def pi_gpio_setup_as_out_and_low(pin):
     gpio.setup(pin, gpio.OUT) 
     gpio.output(pin, gpio.LOW)
 
@@ -84,23 +141,23 @@ def lese_gpios_aus():
     for pin in range(20):
         liste.append(gpio.input(config.GPIOS[pin]))
         l.append(config.GPIOS[pin])
-        #print(config.GPIOS[pin],gpio.input(config.GPIOS[pin]))
-    print(l,liste)   
-def setze_pullup_und_output():
+    return liste # liste mit 1en und 0en je nachdem welcher pin high
+    
+def setze_pullup_und_input():
     for stein in config.ADRESSEN_STEINE.values():
         bus.write_byte_data(stein, config.AKTION.get("GPPUA"), 0xFF)
         bus.write_byte_data(stein, config.AKTION.get("GPPUB"), 0xFF)
         bus.write_byte_data(stein, config.AKTION.get("DIRA"), 0xFF)
         bus.write_byte_data(stein, config.AKTION.get("DIRB"), 0xFF)
 
-def iteration():
+def iteration(litzenzahl):
     steinnummer = 1
     pinnummer = 0
     pinnummerb = 0 
-    litzenzahl = 100  #kommt später von GUI
     gpio_nummer= 0
+    #erstelle_dic(litzenzahl)
     for i in range(litzenzahl):
-        if steinnummer < 4:
+        if steinnummer < 2:
             if pinnummer <= 7: # Pin 1-7 auf Reihe A durchgehen
                 pinnummer +=1
                 string = str(steinnummer) + "A" + str(pinnummer)
@@ -113,9 +170,12 @@ def iteration():
                 pinnummerb = 0
                 string = str(steinnummer) + "A" + str(pinnummer)
         else: # steinnummer = 4
-            if pinnummer < 2: # muss nur bis Pin2 gehen und dann auf Pi switchen
+            if pinnummer <= 7:
                 pinnummer +=1
-                string = str(steinnummer) + "A" + str(pinnummer)   
+                string = str(steinnummer) + "A" + str(pinnummer)
+            elif pinnummerb < 6: # muss nur bis Pin2 gehen und dann auf Pi switchen
+                pinnummerb +=1
+                string = str(steinnummer) + "B" + str(pinnummerb)   
             else: # switche auf Pi
                 if gpio_nummer < 20: #alle 20 pins durchgehen
                     string = "r" + "p" + str(config.GPIOS[gpio_nummer])
@@ -123,15 +183,40 @@ def iteration():
                 else:
                     break
         adresse, aktion, pin = string_zu_befehl(string)
-        print(adresse, aktion, pin)
+        #print(adresse, aktion, pin)
         setze_pin_auf_low(adresse, aktion, pin)
-        checke_werte()
-       
-bus = setup()
+        checke_auf_durchgang(i)
+    
+def vergleiche_soll_ist():
+    for i in range(litzenzahl):
+        anzahl_verbundener_pins = len(config.VERGLEICH_PINS[i+1])
+        for j in range(anzahl_verbundener_pins):
+            if anzahl_verbundener_pins == 1:
+                print(config.ABSCHLUSSMELDUNGEN.get(4))
+            elif anzahl_verbundener_pins == 2: # zwei verbundene Pins, also korrekt, muss aber geprüft werden auf falsche Verdrahtung
+                if config.VERGLEICH_PINS[i+1][j] != config.INPUTS[i+1]:
+                    #print(config.INPUTS[i+1])
+                    #print(config.VERGLEICH_PINS[i+1][j])
+                    print(config.ABSCHLUSSMELDUNGEN.get(1))
+                elif config.VERGLEICH_PINS[i+1][j] != config.OUTPUTS[i+1]:
+                    print(config.ABSCHLUSSMELDUNGEN.get(3))
+            elif anzahl_verbundener_pins > 2:
+                # mehr als zwei verbundene Pins -> Querschluss
+                # überprüfen dann wo oder so 
+               # if config.VERGLEICH_PINS[i+1][j] != config.INPUTS[i+1]:
+                   # print(config.INPUTS[i+1])
+                   # print(config.VERGLEICH_PINS[i+1][j])  
+                print(config.ABSCHLUSSMELDUNGEN.get(2))
+            else:
+                print(config.ABSCHLUSSMELDUNGEN.get(4))
+    
+bus, litzenzahl = setup()
 # Reihentests funktionieren
 #led_test_Reihe_A("1", "3") 
 #led_test_Reihe_B("1", "4")
 #setze_pin_auf_high("1", "OUTA", "1") #Stein, "OUTA" oder "OUTB", Pinnummer
 #adresse, aktion, pin = string_zu_befehl("1OUTA5") # zieht String auseinander in die Teilstrings
 #setze_pin_auf_high(adresse, aktion, pin) #übergibt Einzelstrings an Methode wie oben
-iteration()
+iteration(litzenzahl)
+pprint(config.VERGLEICH_PINS)
+vergleiche_soll_ist()
--- read_test.py
+++ read_test.py
@@ -22,22 +22,38 @@ from Kabel import KABEL
 # # bei Verbindung von reihe a und reihe b wird ausgegeben an welchem Pin von A was anliegt
 # #print(werte)
 
-b = "00000001"
 
-print(hex(int("00000001")))
+def ermittle_low_pin_aus_bin(b):
+    print(b)
+    for buchstabennummer in range(len(b)):
+        if b[buchstabennummer] == "0":
+            print(8-buchstabennummer)
+           # return buchstabennummer+1
+        else:
+            pass
 
-def setze_pin_auf_high(adresse, aktion, pin):
-    for stein in config.ADRESSEN_STEINE.values():
-        if aktion == "A":
-            bus.write_byte_data(stein, config.AKTION.get("DIRA"), 0x00)
-            bus.write_byte_data(stein, config.AKTION.get("DIRB"), 0x00)
-            bus.write_byte_data(stein, config.AKTION.get("OUTA"), 0xFF)
-            bus.write_byte_data(stein, config.AKTION.get("OUTB"), 0xFF)
-            #bus.write_byte_data(stein, config.AKTION.get("GPPUA"), 0xFF)
-            #bus.write_byte_data(stein, config.AKTION.get("GPPUB"), 0xFF)
-            bus.write_byte_data(stein, config.AKTION.get("DIRA"), 0xFF)
-            bus.write_byte_data(stein, config.AKTION.get("DIRB"), 0xFF)
-       # bus.write_byte_data(config.ADRESSEN_STEINE.get(adresse),config.AKTION.get("GPPUA"), config.PINNUMMERN.get(pin))#config.PINNUMMERN.get(pin))
-        bus.write_byte_data(config.ADRESSEN_STEINE.get(adresse),config.AKTION.get("OUTA"), config.PINNUMMERN.get(pin))#config.PINNUMMERN.get(pin))
-    sleep(2)
-    bus.write_byte_data(config.ADRESSEN_STEINE.get(adresse), config.AKTION.get("OUTA"), 0x00)
+ermittle_low_pin_aus_bin("11111100")
+
+# h = "3"
+# #print(h)
+# scale = 16
+# num_of_bits= 8
+# b = bin(int(h, scale))[2:].zfill(num_of_bits)
+# 
+# print(b)
+
+# def setze_pin_auf_high(adresse, aktion, pin):
+#     for stein in config.ADRESSEN_STEINE.values():
+#         if aktion == "A":
+#             bus.write_byte_data(stein, config.AKTION.get("DIRA"), 0x00)
+#             bus.write_byte_data(stein, config.AKTION.get("DIRB"), 0x00)
+#             bus.write_byte_data(stein, config.AKTION.get("OUTA"), 0xFF)
+#             bus.write_byte_data(stein, config.AKTION.get("OUTB"), 0xFF)
+#             #bus.write_byte_data(stein, config.AKTION.get("GPPUA"), 0xFF)
+#             #bus.write_byte_data(stein, config.AKTION.get("GPPUB"), 0xFF)
+#             bus.write_byte_data(stein, config.AKTION.get("DIRA"), 0xFF)
+#             bus.write_byte_data(stein, config.AKTION.get("DIRB"), 0xFF)
+#        # bus.write_byte_data(config.ADRESSEN_STEINE.get(adresse),config.AKTION.get("GPPUA"), config.PINNUMMERN.get(pin))#config.PINNUMMERN.get(pin))
+#         bus.write_byte_data(config.ADRESSEN_STEINE.get(adresse),config.AKTION.get("OUTA"), config.PINNUMMERN.get(pin))#config.PINNUMMERN.get(pin))
+#     sleep(2)
+#     bus.write_byte_data(config.ADRESSEN_STEINE.get(adresse), config.AKTION.get("OUTA"), 0x00)
